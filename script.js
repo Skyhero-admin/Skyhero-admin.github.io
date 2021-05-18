5fa67589cae22b82ee2282a2aecf5a380035f1a4
Skyhero-admin <smnouman007@gmail.com>
2021-05-18 12:57:19 +0530

    Added onresize()

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 function openMyself(){
-	if (max.matches){
 		document.getElementById("myself").style.visibility="visible";
-
 		document.getElementById("close").style.visibility="visible";
-	}
-	else if (min.matches){
-		document.getElementById("myself").style.visibility="visible";
-
-		document.getElementById("close").style.visibility="hidden";
-
-	}
 }
 function closeMyself() {
-	if(max.matches){
-
 	 	document.getElementById("myself").style.visibility="hidden";
 	 	document.getElementById("close").style.visibility="hidden";
-	}
 }
 
-var max=window.matchMedia("(max-width:1000px)")
-var min=window.matchMedia("(min-width:1000px)")
-
+function checkScreenWidth() {
+	if(screen && screen.width < 1000)
+	 {
+		closeMyself();
+	 }
+	 else if(screen && screen.width >= 1000){
+	 	openMyself()
+	 }
+}
