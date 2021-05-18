f4c69b8a3f1118b1aa6fb969ecd3c385d390df30
ashwinexe <53075480+ashwinexe@users.noreply.github.com>
2021-05-18 11:32:15 +0530

    Refactored javascript functions and added a new function
    Now the functions `openMyself()` and `closeMyself()` will only do the task of opening and closing on clicking the button, where as `checkScreenWidth() `will be responsible for reacting to screen changes

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 function openMyself(){
-	if (max.matches){
 		document.getElementById("myself").style.visibility="visible";
-
 		document.getElementById("close").style.visibility="visible";
-	}
-	else if (min.matches){
-		document.getElementById("myself").style.visibility="visible";
-
-		document.getElementById("close").style.visibility="hidden";
-
-	}
 }
 function closeMyself() {
-	if(max.matches){
-
 	 	document.getElementById("myself").style.visibility="hidden";
 	 	document.getElementById("close").style.visibility="hidden";
-	}
 }
 
-var max=window.matchMedia("(max-width:1000px)")
-var min=window.matchMedia("(min-width:1000px)")
-
+function checkScreenWidth() {
+	if(screen && screen.width < 480)
+	 {
+		document.getElementById("myself").style.visibility="hidden";
+	 	document.getElementById("close").style.visibility="hidden";
+	 }
+	 else
+	 {
+		document.getElementById("myself").style.visibility="visible";
+		document.getElementById("close").style.visibility="visible";
+	 }
+}
